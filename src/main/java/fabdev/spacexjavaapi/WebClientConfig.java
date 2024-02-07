c7cbae708db082ce2d91f3dafeac197cb0647913
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-02-07 22:02:25 +0100

    commented WebClientConfig

--- src/main/java/fabdev/spacexjavaapi/WebClientConfig.java
+++ src/main/java/fabdev/spacexjavaapi/WebClientConfig.java
@@ -1,19 +1,19 @@
-package fabdev.spacexjavaapi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
-
-@Configuration
-public class WebClientConfig {
-
-    @Bean
-    public WebClient webClient() {
-        ExchangeStrategies strategies = ExchangeStrategies.builder()
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(262144)) // Adjust the size as needed (e.g., 10 MB)
-                .build();
-
-        return WebClient.builder().exchangeStrategies(strategies).build();
-    }
-}
-
+//package fabdev.spacexjavaapi;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.function.client.ExchangeStrategies;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//@Configuration
+//public class WebClientConfig {
+//
+//    @Bean
+//    public WebClient webClient() {
+//        ExchangeStrategies strategies = ExchangeStrategies.builder()
+//                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(262144)) // Adjust the size as needed (e.g., 10 MB)
+//                .build();
+//
+//        return WebClient.builder().exchangeStrategies(strategies).build();
+//    }
+//}
+//
